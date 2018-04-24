1bb450a73eb3977ecec4dccd97d6208393715962
Zhao Lei <983361306@qq.com>
2018-04-24 10:33:48 +0800

    变量缓存器封装，加载背景图片

--- Main.js
+++ Main.js
@@ -1,17 +1,38 @@
 
 import{ResourceLoader} from './js/base/ResourceLoader.js';
+import{DataStore} from './js/base/DataStore.js';
 //初始化整个游戏的精灵，作为游戏开始的入口
 export class Main{
     constructor(){
         console.log(window.innerWidth+'----'+window.innerHeight);
         this.canvas=wx.createCanvas();
         this.ctx=this.canvas.getContext('2d');
+        this.dataStore=DataStore.getInstance();
+
         const loader=ResourceLoader.create();
        loader.onLoaded(map=>this.onResourceFirstLoaded(map));
-  //     console.log(loader.map);
-
+        var image=wx.createImage();
+        image.src='./res/background.png';
+        image.onload=()=>{
+            this.ctx.drawImage(
+             image,
+             0,0,
+             image.width,image.height,
+             0,0,
+             image.width,image.height
+            );
+        }
     }
+    //游戏开始之前将一些元素保存在dataStore找
     onResourceFirstLoaded(map){
-        console.log(map);
+        this.dataStore.canvas=this.canvas;
+        this.dataStore.ctx=this.ctx;
+        this.dataStore.res=map;
+        this.init();
+    }
+    //入口初始化
+    init(){
+        //首先重置游戏为没有结束
+        this.dataStore.isGameOver=false;
     }
 }
---
+++ js/base/Sprite.js
@@ -0,0 +1,20 @@
+import {DataStore} from './DataStore.js';
+//精灵的基类，负责初始化精灵加载的资源和大小以及位置
+export class Sprite{
+        /**
+     * img 传入Image对象
+     * srcX 要剪裁的起始X坐标
+     * srcY 要剪裁的起始Y坐标
+     * srcW 剪裁的宽度
+     * srcH 剪裁的高度
+     * x 放置的x坐标
+     * y 放置的y坐标
+     * width 要使用的宽度
+     * height 要使用的高度
+     */
+    constructor(
+
+    ){
+
+    }
+}
